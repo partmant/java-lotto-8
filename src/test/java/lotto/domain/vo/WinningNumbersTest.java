e87720973f27ab86308629729804c7fe14c17435
partmant <ghdckdgml191@gmail.com>
2025-10-31 18:25:29 +0900

    test(WinningNumber): 로또와 당첨번호의 일치 개수 반환 테스트 추가

--- src/test/java/lotto/domain/vo/WinningNumbersTest.java
+++ src/test/java/lotto/domain/vo/WinningNumbersTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public final class WinningNumbersTest {
 
     @DisplayName("유효한 문자열을 입력하면 객체가 정상적으로 생성된다.")
@@ -15,7 +17,7 @@ public final class WinningNumbersTest {
         assertThat(winningNumbers).isNotNull();
     }
 
-    @DisplayName("WinningNumbers가 특정 숫자의 포함 여부를 판단할 수 있다.")
+    @DisplayName("특정 숫자의 포함 여부를 판단한다.")
     @Test
     void 특정_숫자의_포함_여부를_판단한다() {
         WinningNumbers winningNumbers = WinningNumbers.from("1, 2, 3, 4, 5, 6");
@@ -23,5 +25,16 @@ public final class WinningNumbersTest {
         assertThat(winningNumbers.contains(1)).isTrue();
         assertThat(winningNumbers.contains(6)).isTrue();
         assertThat(winningNumbers.contains(7)).isFalse();
+    }
+
+    @DisplayName("로또 번호와 일치 개수를 계산하여 반환한다.")
+    @Test
+    void 로또와_일치하는_번호_개수를_계산한다() {
+        WinningNumbers winningNumbers = WinningNumbers.from("1, 2, 3, 4, 5, 6");
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 10, 11, 12));
+
+        int matchCount = winningNumbers.matchCountWith(lotto);
+
+        assertThat(matchCount).isEqualTo(3);
     }
 }

b319f51b73ba8796d48358642343b1abb3903107
partmant <ghdckdgml191@gmail.com>
2025-10-31 18:18:27 +0900

    test(Rank): 등수 매핑 로직 테스트 작성

---
+++ src/test/java/lotto/domain/RankTest.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public final class RankTest {
+
+    @DisplayName("일치 개수와 보너스 번호 여부에 따라 올바른 Rank를 반환한다.")
+    @ParameterizedTest(name = "[{index}] 일치 개수: {0}, 보너스 여부: {1} → 예상 등수: {2}")
+    @MethodSource("provideMatchCountAndBonusForRank")
+    void 일치_개수와_보너스_여부에_따라_Rank를_반환한다(int matchCount, boolean hasBonus, Rank expectedRank) {
+        Rank result = Rank.valueOf(matchCount, hasBonus);
+
+        assertThat(result).isEqualTo(expectedRank);
+    }
+
+    private static Stream<Arguments> provideMatchCountAndBonusForRank() {
+        return Stream.of(
+                of(6, false, Rank.FIRST),
+                of(5, true, Rank.SECOND),
+                of(5, false, Rank.THIRD),
+                of(4, false, Rank.FOURTH),
+                of(3, false, Rank.FIFTH),
+                of(2, false, Rank.MISS),
+                of(0, false, Rank.MISS)
+        );
+    }
+}

83d5fb837af1c176764c412145e033a7f44d9642
partmant <ghdckdgml191@gmail.com>
2025-10-31 11:51:11 +0900

    feat(WinningNumbers): 당첨 번호 VO 구현
    - 생성 및 번호 포함 판단 기능 구현

---
+++ src/main/java/lotto/domain/vo/WinningNumbers.java
@@ -0,0 +1,24 @@
+package lotto.domain.vo;
+
+import java.util.List;
+import lotto.util.LottoValidator;
+import lotto.util.WinningNumbersParser;
+
+public final class WinningNumbers {
+
+    private final List<Integer> numbers;
+
+    private WinningNumbers(List<Integer> numbers) {
+        this.numbers = List.copyOf(numbers);
+    }
+
+    public static WinningNumbers from(String input) {
+        List<Integer> parsedNumbers = WinningNumbersParser.parse(input);
+        LottoValidator.validate(parsedNumbers);
+        return new WinningNumbers(parsedNumbers);
+    }
+
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+}

4025bd453bccc79b4885641f86975931a87d0862
partmant <ghdckdgml191@gmail.com>
2025-10-31 21:34:58 +0900

    test(Lotto): Lotto의 당첨번호 일치 및 보너스 번호 판별 로직 테스트 작성

--- src/test/java/lotto/domain/vo/LottoTest.java
+++ src/test/java/lotto/domain/vo/LottoTest.java
@@ -9,9 +9,36 @@ import org.junit.jupiter.api.Test;
 
 class LottoTest {
 
-    @DisplayName("유효한 6개의 번호로 Lotto 객체를 생성하면 정상적으로 생성된다.")
+    @DisplayName("당첨 번호와 일치하는 개수를 정확히 계산한다.")
     @Test
-    void 유효한_번호로_생성하면_정상적으로_생성된다() {
-        assertThat(new Lotto(List.of(1, 2, 3, 4, 5, 6))).isNotNull();
+    void 당첨번호와_일치개수를_계산한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1, 2, 7, 8, 9, 10);
+
+        int matchCount = lotto.countMatchingNumbers(winningNumbers);
+
+        assertThat(matchCount).isEqualTo(2);
+    }
+
+    @DisplayName("로또에 보너스 번호가 포함되어 있으면 true를 반환한다.")
+    @Test
+    void 보너스번호가_포함되어있으면_true를_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = BonusNumber.from("6");
+
+        boolean hasBonus = lotto.hasBonus(bonusNumber);
+
+        assertThat(hasBonus).isTrue();
+    }
+
+    @DisplayName("로또에 보너스 번호가 포함되어 있지 않으면 false를 반환한다.")
+    @Test
+    void 보너스번호가_없으면_false를_반환한다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        BonusNumber bonusNumber = BonusNumber.from("7");
+
+        boolean hasBonus = lotto.hasBonus(bonusNumber);
+
+        assertThat(hasBonus).isFalse();
     }
 }
